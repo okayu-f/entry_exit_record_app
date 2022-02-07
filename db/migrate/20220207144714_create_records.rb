cedccecdf4f4ff7d216f1aab780431acb2b858ff
yu <yu@yu-MacBookAir.local>
2022-02-07 23:49:50 +0900

    add Record model

---
+++ db/migrate/20220207144714_create_records.rb
@@ -0,0 +1,10 @@
+class CreateRecords < ActiveRecord::Migration[7.0]
+  def change
+    create_table :records do |t|
+      t.references :user, null: false, foreign_key: true
+      t.datetime :exit_at
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_151006) do
+ActiveRecord::Schema.define(version: 2022_02_07_144714) do
+
+  create_table "records", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "exit_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,4 +26,5 @@ ActiveRecord::Schema.define(version: 2022_02_05_151006) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "records", "users"
 end
