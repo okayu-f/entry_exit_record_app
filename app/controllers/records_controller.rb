0e606ab75014b7b6a87a1d1125e89961a5d42837
yu <yu@yu-MacBookAir.local>
2022-02-08 23:28:19 +0900

    records finish provisional ver

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,29 @@
 class RecordsController < ApplicationController
   def entry
+    @user = User.find_by(name: params[:name])
+    @record = @user.records.build()
+    if @record.save
+      flash[:success] = "入室時間が記録されました"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def exit
+    @user = User.find_by(name: params[:name])
+    @record = Record.where(exit_at: nil).find_by(user_id:@user.id)
+    @record.exit_at = DateTime.now
+    if @record.save
+      flash[:success] = "退室時間が記録されました"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
+
+  def index
+    @records = Record.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/help'
-  post '/entry',   to: 'records#entry'
-  post '/exit',   to: 'records#exit'
+  post '/entry',        to: 'records#entry'
+  post '/exit',         to: 'records#exit'
+  get '/records/index', to: 'records#index'
   resources :users
   # resources :records, only: [:create]
 end
