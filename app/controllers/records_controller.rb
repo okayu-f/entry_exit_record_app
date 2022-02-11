a4e77cd0efedd557b346e378ba80a9c849405011
yu <yu@yu-MacBookAir.local>
2022-02-11 17:28:31 +0900

    improve error message

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,27 +1,49 @@
 class RecordsController < ApplicationController
   def entry
     @user = User.find_by(name: params[:name])
-    @record = @user.records.build()
-    if @record.save
-      flash[:success] = "入室時間が記録されました"
-      redirect_to root_url
+    if @user
+      @record = Record.where(exit_at: nil).find_by(user_id:@user.id)
+      if @record
+        flash[:success] = "ユーザーはすでに入室しています。"
+        redirect_to root_url
+      else
+        @record = @user.records.build()
+        if @record.save
+          flash[:success] = "入室時間が記録されました"
+          redirect_to root_url
+        else
+          render 'static_pages/home'
+        end
+      end
     else
-      render 'static_pages/home'
+      flash[:success] = "ユーザーが見つかりませんでした"
+      redirect_to root_url
     end
   end
 
   def exit
     @user = User.find_by(name: params[:name])
-    @record = Record.where(exit_at: nil).find_by(user_id:@user.id)
-    @record.exit_at = DateTime.now
-    if @record.save
-      flash[:success] = "退室時間が記録されました"
-      redirect_to root_url
+    if @user
+      @record = Record.where(exit_at: nil).find_by(user_id:@user.id)
+      if @record
+        @record.exit_at = DateTime.now
+        if @record.save
+          flash[:success] = "退室時間が記録されました"
+          redirect_to root_url
+        else
+          render 'static_pages/home'
+        end
+      else
+        flash[:success] = "ユーザーは入室していません"
+        redirect_to root_url
+      end
     else
-      render 'static_pages/home'
+      flash[:success] = "ユーザーが見つかりませんでした"
+      redirect_to root_url
     end
   end
 
+# ユーザーが居るかいないか判定リファクタリングできる
   def index
     @records = Record.all
   end
